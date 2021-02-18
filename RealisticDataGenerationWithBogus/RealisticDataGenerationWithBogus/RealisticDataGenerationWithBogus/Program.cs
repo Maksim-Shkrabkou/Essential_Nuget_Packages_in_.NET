ce502faf8fcf425b4e4ea3bd5815e70e16d09536
Maksim Shkrabkou <m.shkrabkou@gmail.com>
2021-02-18 00:23:46 -0500

    Realistic data generation with Bogus was added

---
+++ RealisticDataGenerationWithBogus/RealisticDataGenerationWithBogus/RealisticDataGenerationWithBogus/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Text.Json;
+using System.Threading.Tasks;
+using Bogus;
+
+namespace RealisticDataGenerationWithBogus
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            /*
+             * Creating deterministic data (fake data same each run)
+             * Randomizer.Seed = new Random(2342134);
+             */
+            
+            var billingDetailsFaker = new Faker<BillingDetails>()
+                .RuleFor(x => x.CustomerName, x => x.Person.FullName)
+                .RuleFor(x => x.Email, x => x.Person.Email)
+                .RuleFor(x => x.Phone, x => x.Person.Phone)
+                .RuleFor(x => x.AddressLine, x => x.Address.StreetAddress())
+                .RuleFor(x => x.City, x => x.Address.City())
+                .RuleFor(x => x.Country, x => x.Address.Country())
+                .RuleFor(x => x.PostCode, x => x.Address.ZipCode());
+
+            var orderFaker = new Faker<Order>()
+                .RuleFor(x => x.Id, Guid.NewGuid)
+                .RuleFor(x => x.Currency, x => x.Finance.Currency().Code)
+                .RuleFor(x => x.Price, x => x.Finance.Amount(5, 100))
+                .RuleFor(x => x.BillingDetails, x => billingDetailsFaker);
+
+            foreach (var order in orderFaker.GenerateForever())
+            {
+                var text = JsonSerializer.Serialize(orderFaker.Generate());
+                Console.WriteLine(text);
+                await Task.Delay(1000);
+            }
+            
+            /*
+            var text = JsonSerializer.Serialize(orderFaker.Generate());
+            Console.WriteLine(text);
+            */
+        }
+    }
+}
